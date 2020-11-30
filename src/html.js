1353437f6286ff93ca31791223f543bb8fef946c
Liron de Castro <liron.dco@gmail.com>
2020-11-29 23:44:07 -0800

    more corrections

--- src/html.js
+++ src/html.js
@@ -8,8 +8,7 @@ const generateHtml = function(htmlList) {
         <button id="new-button" class="new-button">Add New</button>
     </div>
     <select name="filter" id="filter" class="filter">
-    <option>Filter By Rating</option> 
-    <option value="0" ${(store.storeItems.filter === '0') ? 'selected' : ''}>Show All</option>
+    <option value="0" ${(store.storeItems.filter === '0') ? 'selected' : ''}>Filter by Rating</option>
     <option value="1" ${(store.storeItems.filter === '1') ? 'selected' : ''}>Hide unrated bookmarks</option>
     <option value="2" ${(store.storeItems.filter === '2') ? 'selected' : ''}>Show only 2 stars and up</option>
     <option value="3" ${(store.storeItems.filter === '3') ? 'selected' : ''}>Show only 3 stars and up</option>
